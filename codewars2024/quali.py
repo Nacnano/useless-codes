56b8aff9b91e775ec024be81d1b17fe4b5522a83
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-02-03 14:50:46 +0700

    codewar

---
+++ codewars2024/quali.py
@@ -0,0 +1,15 @@
+data = {}
+
+ret = 0
+
+
+for i in range(2000, 4001, 12):
+    tmp = i
+    dig = 0
+    while tmp>0:
+        dig += tmp % 10
+        tmp = tmp // 10
+    if dig == 8:
+        ret +=1
+
+print(ret)
---
+++ codewars2024/qualification.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int max = 1e5+10;
+int a
+
+int n, m;
+
+void solve() {
+
+    cin >> n >> m;
+
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
