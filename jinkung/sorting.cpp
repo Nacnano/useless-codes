59e02e00545d833af4c9b8bf4190edd97017710b
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2024-12-24 14:45:47 +0700

    sort

--- jinkung/sorting.cpp
+++ jinkung/sorting.cpp
@@ -9,8 +9,12 @@ int main(){
     cin >> n;
     for(int i=0;i<n;i++) cin >> arr[i];
 
-    // sorting
-
+    // bubble sort
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j] > arr[j+1]) swap(arr[j], arr[j+1]);
+        }
+    }
 
     for(int i=0;i<n;i++) cout << arr[i] << " ";
 }
