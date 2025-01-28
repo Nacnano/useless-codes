d0218f75e907f8b2cca8caf9fdd7b1090e2f15f0
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-01-28 23:00:37 +0700

    add posn2

--- posn2/example.cpp
+++ posn2/example.cpp
@@ -1,14 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long arr[100];
 
 int main(){
     ios_base::sync_with_stdio(0), cin.tie(NULL);
 
-    int n
-    cin >> n;
+    int m, n;
+    cin >> m >> n;
+    for(int i=0;i<m;i++){
+        cin >> arr[i];
+    }
 
-    //something
+    long long l = 1, r = 1e12;
+    while(r - l > 0){
+        // Time
+        long long mid = (l + r + 1) / 2;
+        // Quantity
+        long long q = 0;
+        for(int i=0;i<m;i++){
+            q += mid/arr[i];
+        }
+        cout << l << " " << r << " " << q << endl;
+
+        if(q > n){
+            r = mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+
+    cout << l;
 
     return 0;
 }
