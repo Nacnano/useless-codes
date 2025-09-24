911de0182863811f5c8153769bcb9f1ea37b941b
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-09-24 11:37:50 +0700

    add

---
+++ monkey/pattern2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for(int level=1;level<=n;level++){
+        for(int star=1;star<=level;star++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
