a3ab97e5bca49021925f4bff31a006f469740340
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2026-02-11 14:06:44 +0700

    add

--- intern_problems/scb_coop2025.py
+++ intern_problems/scb_coop2025.py
@@ -54,26 +54,3 @@ print(answer)
 #     x += 
 #     y += 
     
-    
-    
-    
-# 5 Cards
-# Random a card. Expected number of draws to get all 5 cards.
-
-
-
-# 5 * (1/1 + 1/2 + 1/3 + 1/4 + 1/5)
-
-# 1
-
-# 1 * 1
-
-# 2
-
-# 2 * (1/1 + 1/2)
-
-# 2 * 1 + 1
-
-# 3 * (1/1 + 1/2 + 1/3)
-
-# 3 * 1 + 3 * (1/2) + 3 * (1/3)
